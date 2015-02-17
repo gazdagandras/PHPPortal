856116ce4d761f8684ba468b9ff1dd0ec6a9d8f5
Andras Gazdag <gazdag.andras@gmail.com>
2015-02-17 14:07:54 +0100

    Fejléc formázása

--- views/includes/header.php
+++ views/includes/header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP Portál - <?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" type="text/css" href="views/css/base.css">
     
     <!-- jQuery: -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -16,6 +15,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="views/css/base.css">
+
   </head>
   <body>
 
