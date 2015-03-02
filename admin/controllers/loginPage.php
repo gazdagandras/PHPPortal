f2e33649946322e1dc8ae1b1ce4996995ba17a85
Ervin Papp <ervin.papp@gmail.com>
2015-03-02 19:34:02 +0100

    Admin - hashed password

--- admin/controllers/loginPage.php
+++ admin/controllers/loginPage.php
@@ -22,7 +22,8 @@ if (isset($_POST['loginSubmit'])) {
 	// kiszedem az eredményből az 1db sort:
 	$uData = $result->fetch_array();
 	
-	if ($uData['upass'] == $uPass) $success = true;
+	//if ($uData['upass'] == $uPass) $success = true; //hash nélküli jelszók ellenőrzése
+	if ($uData['upass'] == crypt($uPass, $uData['upass'])) $success = true;  //hashed password ellenőrzése
 	
 	if ($success) {
 		// admin oldalakhoz hozzáférés
