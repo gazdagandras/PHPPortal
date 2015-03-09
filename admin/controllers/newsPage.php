8d2eb92633cd798a6759f38812ff9465c2a49ec8
Ervin Papp <ervin.papp@gmail.com>
2015-03-09 22:13:58 +0100

    Admin - keresés hírekben, hírek utólagos publikálása

--- admin/controllers/newsPage.php
+++ admin/controllers/newsPage.php
@@ -1,6 +1,7 @@
 <?php
 
 $pageTitle = "Hírek rögzítése";
+$found = array();
 
 
 //rovatok kigyűjtése:
@@ -13,12 +14,50 @@ if ($db->errno) {
 $tags = array();
 $c = 0;
 while ($tagData = $result->fetch_array()) {
-	$tags[$c]['id'] = $tagData['id'];
-	$tags[$c]['description'] = $tagData['description'];
+	$id = $tagData['id'];
+	$tags[$id] = $tagData['description'];
 	$c++;
 }
 
-// login form feldolgozása:
+//még nem publikált hírek kigyűjtése:
+$query = "SELECT * FROM `news` WHERE published=0";
+$nonPublished = $db->query($query);
+if ($db->errno) {
+	die($db->error);
+}
+
+// hír keresés form feldolgozása:
+if (isset($_POST['searchSubmit'])) {
+  
+	$where = '';
+	$newsTag = $_POST['tag'];
+	if ($newsTag > 0) $where.= "tag=$newsTag";
+	$newsTitle = $_POST['title'];
+	if (!empty($newsTitle)) $where.= (!empty($where)) ? " AND title LIKE '%$newsTitle%'"  : "title LIKE '%$newsTitle%'";
+	$newsText = $_POST['text'];
+	if (!empty($newsText)) $where.= (!empty($where)) ? " AND `text` LIKE '%$newsText%'"  : "`text` LIKE '%$newsText%'";
+	 
+	$query = (!empty($where)) ? "SELECT * FROM `news` WHERE $where" : "SELECT * FROM `news` LIMIT 10";
+	$found = $db->query($query);
+	if ($db->errno) {
+		die($db->error);
+	}
+	
+	$_SESSION['sresult'] = array();
+	$c = 0;
+	while ($newsData = $found->fetch_array()) {
+		$_SESSION['sresult'][$c]['tag'] = $newsData['tag'];
+		$_SESSION['sresult'][$c]['title'] = $newsData['title'];
+		$_SESSION['sresult'][$c]['text'] = $newsData['text'];
+		$_SESSION['sresult'][$c]['date'] = $newsData['date'];
+		$c++;
+	}	
+		
+	header("Location: $HOST/admin/?q=hirek");
+	exit;
+}
+
+// hír rögzítése form feldolgozása:
 if (isset($_POST['newsSubmit'])) {
   
 	$newsTitle = $_POST['title'];
@@ -37,6 +76,27 @@ if (isset($_POST['newsSubmit'])) {
 	
 	$_SESSION['msg'] = 'Hír rögzítve.';
 		
+	header("Location: $HOST/admin/?q=hirek");
+	exit;
+}
+
+// hír utólagos publikálása form feldolgozása:
+if (isset($_POST['publishSubmit'])) {
+  
+	//var_dump($_POST); die;
+	foreach ($_POST as $k => $v) {
+		//var_dump($k, $v);
+		if ($k != 'publishSubmit') {
+			$query = "UPDATE news SET published=1 WHERE id=$v";
+			//var_dump($query); die;
+			$result = $db->query($query);
+			if ($db->errno) {
+				die($db->error);
+			}
+		}
+	}
+	
+	//die('Nyekk');
 	header("Location: $HOST/admin/?q=hirek");
 	exit;
 }
--- admin/views/newsPage.php
+++ admin/views/newsPage.php
@@ -1,6 +1,58 @@
 <?php include('includes/header.php'); ?>
 
 <div id="content">
+  <h2>Keresés a hírekben</h2>
+  
+	<?php if (isset($_SESSION['sresult'])) : ?>
+	
+		<?php 
+			if (!empty($_SESSION['sresult'])) {
+				foreach ($_SESSION['sresult'] as $item) {
+					$tagID = $item['tag'];
+					echo '<div class="news">';
+					echo '<div class="tag">'.$tags[$tagID].'</div>';
+					echo '<div class="title">'.$item['title'].'</div>';
+					echo '<div class="text">'.$item['text'].'</div>';
+					echo '<div class="date">'.$item['date'].'</div>';
+					echo '</div>';
+				}
+			} else {
+				echo '<p>Nincs találat.</p>';
+			}
+			unset($_SESSION['sresult']); 
+		?>
+		<br>
+		<ul id="navigation" class="nav nav-pills">
+		<li role="presentation"><a href="?q=hirek">Új keresés</a></li>
+		</ul>
+	
+	<?php else : ?>
+  
+		<form name="searchForm" method="post" id="searchForm">
+				<label>Rovatban:</label>
+				<br>
+				<select name="tag">
+				<option value="0">Összes</option>'
+				<?php 
+					foreach ($tags as $id => $tag) {
+						echo '<option value="'.$id.'">'.$tag.'</option>';
+					}
+				?>
+				</select>
+				<br>
+				<label>Címben:</label>
+				<br>
+				<input type="text" name="title" class="shortText">
+				<br>
+				<label>Szövegtörzsben:</label>
+				<br>
+				<input type="text" name="text" class="shortText">
+				<br>
+				<input type="submit" name="searchSubmit" value="Keresés">
+			</form>
+  
+	<?php endif; ?>
+  <hr>
   <h2>Hírek rögzítése</h2>
   
 	<?php if (isset($_SESSION['msg'])) : ?>
@@ -36,8 +88,8 @@
 				<br>
 				<select name="tag">
 				<?php 
-					foreach ($tags as $tag) {
-						echo '<option value="'.$tag['id'].'">'.$tag['description'].'</option>';
+					foreach ($tags as $id => $tag) {
+						echo '<option value="'.$id.'">'.$tag.'</option>';
 					}
 				?>
 				</select>
@@ -54,6 +106,31 @@
 		
 			<p>Nincs jogosultsága az oldal megtekintéséhez.</p>
 		
+		<?php endif; ?>
+		
+		<?php if ($_SESSION['rights']==1 || $_SESSION['rights']==2) : ?>
+		
+			<hr>
+			<h2>Publikálatlan hírek</h2>
+			
+			<form name="publishForm" method="post" id="publishForm">
+				<table>
+					<?php
+						$c = 1;
+						foreach ($nonPublished as $item) {
+							$tagID = $item['tag'];
+							echo '<tr>';
+							echo '<td class="tid"><input type="checkbox" name="publish'.$c.'" value="'.$item['id'].'"></td>';
+							echo '<td class="ttag">'.$tags[$tagID].'</td>';
+							echo '<td class="ttitle">'.$item['title'].'</td>';
+							echo '</tr>';
+							$c++;
+						}
+					?>
+				</table>
+				<input type="submit" name="publishSubmit" value="Publikál">
+			</form>
+			
 		<?php endif; ?>
   
 	<?php endif; ?>
